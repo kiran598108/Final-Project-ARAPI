4334ec6a44fef343a34e47d241b486c45e996c29
kiran598108 <saikirantirumalasetty3098@gmail.com>
2025-02-19 21:58:00 +0000

    commit

--- server/index.js
+++ server/index.js
@@ -10,10 +10,13 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
-mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+
+
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/test";
+
+mongoose.connect(mongoURI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
 // Routes
 app.use('/api/auth', require('./middleware/auth'));
