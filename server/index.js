d323c1f2afdd879e2a60b63d3f4324871d19d594
kiran598108 <saikirantirumalasetty3098@gmail.com>
2025-02-21 17:34:07 +0000

    commit

--- server/index.js
+++ server/index.js
@@ -4,20 +4,21 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 
-mongoose.connect('mongodb+srv://saikiran598108:Once@time@657@cluster0.mongodb.net/test')
+mongoose.connect(process.env.MONGODB_URI ||'mongodb+srv://saikiran598108:Once@time@657@cluster0.mongodb.net/test')
 .then(() => console.log("Connected to MongoDB"))
 .catch(err => console.error("MongoDB connection error:", err));
 
 // Routes
 app.use('/api/auth', require('./middleware/auth'));
 app.use('/api/recipes', require('./routes/recipes'));
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
